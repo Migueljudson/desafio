1ede16ea0668ce9b8816ea8a39e44c6220f7e577
Migueljudson <migueljudson27@gmail.com>
2025-05-22 17:23:09 +0000

    AULA DO DIA 22/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import './App.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+   <div>
+    <h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Fuga itaque suscipit necessitatibus, optio explicabo, 
+        maxime quas quasi est ea repellendus,
+        repudiandae obcaecati cumque facilis soluta exercitationem quod corrupti tenetur officiis?</p>
+    </h1>
+   </div>
   );
-}
+ }
 
-export default App;
