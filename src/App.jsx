6f9ecf1a80e5bf795558fbdea81d0d1387b6f656
Migueljudson <migueljudson27@gmail.com>
2025-06-06 17:08:34 +0000

    desafio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyText } from "./components/MyText";
+// import { MyTextList } from "./components/MyTextList";
+import { MyGrid } from "./components/MyGrid";
 
 export default function App() {
+
   return (
     // React Fragment
-    <>  
-      <MyText />
-      <MyText />
-      <MyText />
+    <>
+      <MyGrid />
     </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,21 +1,12 @@
-import Style from './MyText.module.css'; 
-import function MyText() {
-    return(
-        <div className={Style.text}>
-            <h1>MEU PRIMEIRO PROJETO</h1> <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Fuga itaque suscipit necessitatibus, optio explicabo, 
-                maxime quas quasi est ea repellendus,
-                repudiandae obcaecati cumque facilis soluta exercitationem quod corrupti tenetur officiis?</p>
-        </div>
-    )
-    
-}
+import styles from "./MyText.module.css";
 
-export function MyText({ children }) {
+export function MyText({ title, children }) {
   return (
-    <div className="text-2xl text-blue-500 font-bold">
-      {children}
+    <div className={styles.container}>
+      <div className={styles.div}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
+      </div>
     </div>
   );
 }
