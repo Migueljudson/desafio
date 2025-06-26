bdd3305f3210255df5e91c9d30abb576b062bce8
Migueljudson <migueljudson27@gmail.com>
2025-06-26 16:47:47 +0000

    desafio da etapa 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyTextList } from "./components/MyTextList";
-import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-import { Footer } from "./components/Footer";
 import { LuckyNumber } from "./components/etapa2/LuckyNumber";
 
 export default function App() {
   return (
     <>
-        <Header />
         <LuckyNumber />
     </>
   );
