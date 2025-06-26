9b0f8316e2c52130c700c8319f7991f4c1feba3d
Migueljudson <migueljudson27@gmail.com>
2025-06-26 16:43:51 +0000

    desfio da etapa 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { MyTextList } from "./components/MyTextList";
-import { MyGrid } from "./components/MyGrid";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { LuckyNumber } from "./components/etapa2/LuckyNumber";
 
 export default function App() {
   return (
     <>
-      <MyGrid />
+        <Header />
+        <LuckyNumber />
     </>
   );
 }
