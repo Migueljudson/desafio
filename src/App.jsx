ce0c11705653fb408bfdc937384b3450ca7f513c
Migueljudson <migueljudson27@gmail.com>
2025-07-18 16:29:07 +0000

    aula do dia 17/07/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyTextList } from "./components/MyTextList";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
 import { LuckyNumber } from "./components/etapa2/LuckyNumber";
+import { ProductList } from "./components/etapa2/ProductList";
+import { useState } from "react";
 
 export default function App() {
+  
+  const [cart, setCart] = useState([]);
+
+  function addToCart(product) {
+    setCart((prevCart) => [...prevCart, product]);
+  }
+
+  function removeFromCart(product) {
+    setCart((prevCart) => {
+    const index = prevCart.findIndex((item) => item.id === product.id);
+    if (index === -1) return prevCart;
+    const newCart = [...prevCart];
+    newCart.splice(index, 1); // remove apenas o primeiro encontrado
+    return newCart;
+  });
+  }
   return (
     <>
-        <LuckyNumber />
+      <Header cart={cart}/>
+      <ProductList addToCart={addToCart} removeFromCart={removeFromCart}/>
     </>
   );
 }
